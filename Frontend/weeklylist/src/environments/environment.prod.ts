9d450c74bf68f26c7fc75329b6d74b33718fda53
JacopoMoro <jacopomoro04@gmail.com>
2026-08-17 11:50:08 +0200

    Commit per deploy vercel + render

--- Frontend/weeklylist/src/environments/environment.prod.ts
+++ Frontend/weeklylist/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  backendUrl: 'https://weeklylistv2-6f391fbc0f5d.herokuapp.com/api/v1',
+  backendUrl: 'https://wlpizzeria.onrender.com/api/v1',
   localStorageTokenKey: 'token_auth',
   localStorageIsOwner : 'isOwner',
   localStorageEmail: 'email',
   localStorageFirstName: 'firstName',
   localStorageLastName: 'lastName',
-  urlWebSite : "https://weeklylistv2frontend-28ef29d77003.herokuapp.com"
+  urlWebSite : ""
 };

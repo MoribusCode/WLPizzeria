baa84eb224a71048c4f50a66449605971c0453a3
JacopoMoro <jacopomoro04@gmail.com>
2026-08-17 12:10:47 +0200

    .

--- Frontend/weeklylist/src/environments/enviroment.ts
+++ Frontend/weeklylist/src/environments/enviroment.ts
@@ -1,7 +1,7 @@
 export const environment = {
 
   production: false,
-  backendUrl: 'http://127.0.0.1:3000/api/v1',
+  backendUrl: 'https://wlpizzeria.onrender.com/api/v1',
   //backendUrl: 'http://localhost:3000/api/v1',
   //backendUrl: 'https://weeklylistv2-6f391fbc0f5d.herokuapp.com/api/v1',
   localStorageTokenKey: 'token_auth',
@@ -9,7 +9,7 @@ export const environment = {
   localStorageEmail: 'email',
   localStorageFirstName: 'firstName',
   localStorageLastName: 'lastName',
-  urlWebSite : "http://localhost:4200"
+  urlWebSite : "https://wl-pizzeria.vercel.app"
   //urlWebSite : "https://weeklylistv2frontend-28ef29d77003.herokuapp.com"
   //urlWebSite : "http://127.0.0.1:8080/"
 
